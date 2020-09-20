24bdbf5a0fdd92b22823026e111d5f06fd41e0e2
Sivecano <49620143+Sivecano@users.noreply.github.com>
2020-09-20 23:02:57 +0200

    Estus is Food

--- src/main/kotlin/mymod/MyItems.kt
+++ src/main/kotlin/mymod/MyItems.kt
@@ -2,6 +2,7 @@ package mymod
 
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.FoodComponent
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
@@ -10,8 +11,9 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
+import kotlin.reflect.jvm.internal.impl.types.AbstractTypeCheckerContext
 
-val estusSettings = Item.Settings().group(ItemGroup.FOOD).maxCount(10)
+val estusSettings = Item.Settings().group(ItemGroup.FOOD).maxCount(10).food(FoodComponent.Builder().alwaysEdible().hunger(0).build())
 val ESTUS_FLASK = EstusFlask(estusSettings)
 
 class EstusFlask(settings: Settings): Item(settings) {
