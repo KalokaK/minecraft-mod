b3993b58aee5cb3575d230747fb54e0fa17bd302
Sivecano <49620143+Sivecano@users.noreply.github.com>
2020-09-20 22:49:11 +0200

    Estus Flask works now :)

--- src/main/kotlin/mymod/MyItems.kt
+++ src/main/kotlin/mymod/MyItems.kt
@@ -1,6 +1,5 @@
 package mymod
 
-import com.sun.javafx.binding.Logging
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
@@ -12,29 +11,17 @@ import net.minecraft.util.TypedActionResult
 import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 
-val estusSettings = Item.Settings().group(ItemGroup.FOOD).maxCount(10).group(ItemGroup.COMBAT)
+val estusSettings = Item.Settings().group(ItemGroup.FOOD).maxCount(10)
 val ESTUS_FLASK = EstusFlask(estusSettings)
-val LOGGER = Logging.getLogger()
 
 class EstusFlask(settings: Settings): Item(settings) {
     override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
-        LOGGER.info("use on relaease estus is being called")
         user?.heal(10.0F)
         user?.itemCooldownManager?.set(this, 20)
+        user!!.getStackInHand(hand).decrement(1)
         return super.use(world, user, hand)
     }
-    override fun finishUsing(stack: ItemStack?, world: World?, user: LivingEntity?): ItemStack {
-        LOGGER.info("use on relaease estus is being called")
-        user?.heal(10.0F)
-        if (user is PlayerEntity) {
-            user.itemCooldownManager.set(this, 20)
-        }
 
-        return super.finishUsing(stack, world, user)
-    }
-    override fun isFood(): Boolean {
-        return false
-    }
     override fun isUsedOnRelease(stack: ItemStack?): Boolean {
         return true
     }
--- src/main/kotlin/mymod/MyMod.kt
+++ src/main/kotlin/mymod/MyMod.kt
@@ -6,6 +6,7 @@ import net.minecraft.item.ItemGroup
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
+
 @Suppress("unused")
 fun init() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -14,6 +15,5 @@ fun init() {
 
     registerItems()
 
-    LOGGER.info("init main still works")
+    println("init main still works")
 }
-
